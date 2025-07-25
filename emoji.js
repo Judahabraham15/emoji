c7f71f36edc63a5f278e7c18e8ed632854b903fb
Judah Abraham <judahabrahamforgood@gmail.com>
2025-07-25 19:25:17 +0100

    Another project completed while i was bored😪

--- emoji.js
+++ emoji.js
@@ -2,6 +2,7 @@ const emoji = document.querySelector(".emoji");
 
 const emojis = ["🤝", "😪", "💚", "😉", "🔥", "🧑🏿‍💻"];
 
+
 emoji.addEventListener("mouseover", () => {
   emoji.innerHTML = emojis[Math.floor(Math.random() * emojis.length)];
 });
